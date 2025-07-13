5c648d642a7b6646a951f06b3d4b089b7258a112
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-07-13 15:06:37 -0300

    ragdoll explosion test added.

---
+++ 67Bits_Project/Assets/Scripts/Animation/AIAnimator.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAnimator : MonoBehaviour
+{
+    [SerializeField] Animator _anim = null;
+    [SerializeField] Rigidbody _handleRb = null;
+    [SerializeField] List<Rigidbody> _rigidbodies = null;
+    [SerializeField] List<Collider> _colliders = null;
+
+    private void Awake()
+    {
+        DisableRagdoll();
+    }
+
+    public float _explosionForce = 1f;
+
+    [ContextMenu(nameof(Explode))]
+    public void Explode()
+    {
+        EnableRagdoll();
+
+        int _count = _rigidbodies.Count;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _rigidbodies[i].AddExplosionForce(_explosionForce, transform.position + transform.forward, 2f, 1f, ForceMode.Impulse);
+        }
+
+        Invoke(nameof(DisableRagdoll), 5f);
+    }
+
+    [ContextMenu(nameof(EnableRagdoll))]
+    public void EnableRagdoll()
+    {
+        //_handleRb.isKinematic = true;
+        _anim.enabled = false;
+        SetRagdoll(true);
+    }
+
+    [ContextMenu(nameof(DisableRagdoll))]
+    public void DisableRagdoll()
+    {
+        var _worldPosition = _handleRb.position;
+        _worldPosition.y = 0;
+        transform.position = _worldPosition;
+
+        _anim.enabled = true;
+        _handleRb.isKinematic = false;
+        SetRagdoll(false);
+    }
+
+    public void SetRagdoll(bool _enable)
+    {
+        int _count = _rigidbodies.Count;
+
+        for (int i = 0; i < _count; i++)
+        {
+            _rigidbodies[i].isKinematic = !_enable;
+            _colliders[i].enabled = _enable;
+        }
+    }
+}
--- 67Bits_Project/Assets/Scripts/Movement/PlayerInput.cs
+++ 67Bits_Project/Assets/Scripts/Movement/PlayerInput.cs
@@ -3,14 +3,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] InputActionReference _move = null;
-    [SerializeField] InputActionReference _jump = null;
+    [SerializeField] InputActionReference _moveReference = null;
+    [SerializeField] bool _useVirtualControls = false;
+    //[SerializeField] InputActionReference _jump = null;
 
-    public Vector2 Move { get => _move.action.ReadValue<Vector2>(); }
+    [Header("// READONLY")]
+    [SerializeField] Vector2 _virtualInput = default;
+
+    public Vector2 Move { get => GetMoveValue(); }
     //public bool Jump { get => _jump.action.WasPerformedThisFrame(); }
 
     //private void Update()
     //{
-    //    Debug.Log($"{_move.action.ReadValue<Vector2>()}");
+    //    Debug.Log($"{Move}");
     //}
+
+    public void ApplyVirtualMove(Vector2 _value)
+    {
+        Vector2 _direction = _value.normalized;
+        float _clampedMagnitude = Mathf.Clamp(_value.magnitude, 0f, 1f);
+        _virtualInput = _direction * _clampedMagnitude;
+
+    }
+
+    public Vector2 GetMoveValue()
+    {
+        if (_useVirtualControls)
+        {
+            return _virtualInput;
+        }
+        else
+        {
+            return _moveReference.action.ReadValue<Vector2>();
+        }
+    }
 }
